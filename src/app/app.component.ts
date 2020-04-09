74d9560157addad2534cf212097fe754f62eaaeb
alejandro <alejoagudelo23@gmail.com>
2020-04-08 22:42:54 -0500

    se crea el proyecto calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+
+  public botoneslargos :string[]=['AC','CE'];
+  public  botones:string[]=['7','8','9','/','4','5','6','*','1','2','3','-','','.','0','=','+'];
 }
